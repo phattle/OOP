c528669ea312262ea32daa3378b48b2e517b4dc4
Phát Lê <lethanhphat1408@gmail.com>
2020-12-15 18:58:47 +0700

    first commit

---
+++ app/src/main/java/com/example/oop/Animal.java
@@ -0,0 +1,7 @@
+package com.example.oop;
+
+public class Animal {
+    //1 : thuộc tnhs
+    String name;
+    //2 : Hành vi
+}
---
+++ app/src/main/java/com/example/oop/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.oop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        tính chất
+//        tính kế thừa
+//        quan hệ has A
+//        tính đa hình
+//        tính trừu tượng
+//        tính đóng gói
+    }
+}
